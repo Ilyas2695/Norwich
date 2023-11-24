a2a98d0bcb1088fa9c71530ae304d95f7ab0b004
Ilyas <adilkhan.ilyas1510@gmail.com>
2023-11-24 19:09:37 +0000

    removed 'back' button

--- norwichproject/src/Scanner.js
+++ norwichproject/src/Scanner.js
@@ -70,12 +70,8 @@ const Scanner = props => {
     // If you do not specify a target,
     // QuaggaJS would look for an element that matches
     // the CSS selector #interactive.viewport
-    <div>
-      <div id="interactive" className="viewport" />
-        <button type="button" className="btn btn-primary" onClick={onBackClick}>
-          Go Back
-        </button>
-    </div>
+    <div id="interactive" className="viewport" />
+ 
   );
 };
 
