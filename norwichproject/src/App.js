86b04ed3434660d2c8c792554cb71a26199aade3
Ilyas <adilkhan.ilyas1510@gmail.com>
2023-11-24 20:50:42 +0000

    working

--- norwichproject/src/App.js
+++ norwichproject/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import IngredientsSelector from './IngredientsSelector';
 import Scanner from './Scanner';
+import Results from './Results';
 import React, { useState } from 'react';
 
 
@@ -20,22 +21,33 @@ import React, { useState } from 'react';
 function App() {
   const [currentView, setCurrentView] = useState('ingredients');
 
+  const [ingredients, setIngredients] = useState({})
+
   const showScanner = () => {
     setCurrentView('scanner');
   };
 
-  const handleDetected = (code) => {
-    // Handle the barcode code
-    alert(`Barcode detected: ${code}`);
+  const handleDetected = async(value) => {
+    value = "7622210445346";
+    const response = (await fetch(`https://world.openfoodfacts.net/api/v3/product/${value}`));
+    const result = await response.json();
+    
+    
+    const ing = result.product.ingredients.flatMap(i=>i.ingredients ? (i.ingredients.map(i2=>i2.id)):i.id);
+    const normalised = ing.map(i=>i.replace("en:",""))
+    setIngredients(normalised);
     // Optionally switch back to ingredients view or handle accordingly
-    setCurrentView('ingredients');
+    setCurrentView('results');
+    
+  
+  
   };
 
   return (
     <div>
       {currentView === 'ingredients' ? (
-        <IngredientsSelector onScanClick={showScanner} />
-      ) : (
+        <IngredientsSelector onScanClick={handleDetected} />
+      ) : currentView === 'results' ? <Results ingredients={ingredients}/> : (
         <Scanner onDetected={handleDetected} />
       )}
     </div>
